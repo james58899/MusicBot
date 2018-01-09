1ae0bed157660c1b0ab8feb986a31455fadef91d
james58899 <james59988@gmail.com>
2018-01-10 02:10:10 +0800

    telegram audio handle

--- Component/Telegram.js
+++ Component/Telegram.js
@@ -1,8 +1,40 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 class telegram {
-    constructor(config) {
-        this.bot = new TelegramBot(config.discord.token, {polling: true});
+    constructor(config, core) {
+        this.bot = new TelegramBot(config.telegram.token, { polling: true });
+
+        //Command
+        this.bot.onText(/^\/(\w+)@?(\w*)/i, (msg) => {
+            //TODO
+        });
+
+        // Audio
+        this.bot.on('audio', (msg) => {
+            if (!msg.audio.title) {
+                this.bot.sendMessage(msg.chat.id, "這個音樂沒有標題\n請幫它添加一個！", {
+                    reply_to_message_id: msg.message_id,
+                    reply_markup: {
+                        force_reply: true,
+                        selective: true
+                    }
+                }).then((needTitle) => {
+                    this.bot.onReplyToMessage(msg.chat.id, needTitle.message_id, (title) => {
+                        if (title.text) {
+                            this.bot.getFileLink(msg.audio.file_id).then((link) => {
+                                core.add(link, msg.audio.duration, title.text, msg.audio.performer)
+                            });
+                        } else {
+                            this.bot.sendMessage(msg.chat.id, "這看起來不像是標題", {reply_to_message_id: title.message_id})
+                        }
+                    })
+                });
+            } else {
+                this.bot.getFileLink(msg.audio.file_id).then((link) => {
+                    core.add(link, msg.audio.duration, msg.audio.title, msg.audio.performer)
+                });
+            }
+        })
     }
 }
 
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const path = require('path');
 const config = require(path.resolve("Utils/Config"));
 const database = require(path.resolve("Database/SQLite"));
+const URLParser = require(path.resolve("Utils/URLParser"));
 const encoder = new (require(path.resolve("Audio/Encoder"))) (config);
 const discord = new (require(path.resolve("Component/Discord"))) (config);
 const telegram = new (require(path.resolve("Component/Telegram"))) (config);
+
+const core = class {
+    add(file, duration, title, artist) {
+        //TODO
+    }
+}
