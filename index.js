20e179952d599ebc17cd90cfa2d14e3beec872cf
james58899 <james59988@gmail.com>
2018-01-19 22:19:31 +0800

    database

--- index.js
+++ index.js
@@ -1,41 +1,52 @@
 const path = require('path');
-const config = require(path.resolve('Utils/Config'));
-const database = require(path.resolve('Database/SQLite'));
-const URLParser = require(path.resolve('Utils/URLParser'));
+const uuid = require('uuid/v1');
+const MediaInfo = require(path.resolve('Utils/MediaInfo'));
 
 const Core = class {
     constructor() {
-        this.encoder = new (require(path.resolve('Audio/Encoder')))(config);
-        this.discord = new (require(path.resolve('Component/Discord')))(config, this);
-        this.telegram = new (require(path.resolve('Component/Telegram')))(config, this);
+        this.config = require(path.resolve('Utils/Config'));
+        this.encoder = new (require(path.resolve('Audio/Encoder')))(this.config);
+        this.database = new (require(path.resolve('Database/SQLite')))(this.config);
+        this.discord = new (require(path.resolve('Component/Discord')))(this.config, this);
+        this.telegram = new (require(path.resolve('Component/Telegram')))(this.config, this);
     }
 
     /**
-    * Add sound to detabase
-    *
-    * @param {string} file
-    * @param {number} duration
-    * @param {string} title
-    * @param {string|undefined} artist
-    */
-    async add(file, duration, title, artist) {
-        const encodedFile = await this.encoder.encode(file, 'test');
+     * Add sound to detabase
+     *
+     * @param {String} sender
+     * @param {String} input
+     * @param {?String} title
+     * @param {?String} artist
+     * @param {?Number} duration
+     * @return {Object}
+     */
+    async addSound(sender, input, title, artist, duration) {
+        // I think get media info will faster then encode...
+        const mediainfo = new MediaInfo(input);
+        const file = await this.encoder.encode(input, `${sender}_${uuid()}.opus`);
+
+        if (!title) title = mediainfo.getTitle();
+        if (!artist) artist = mediainfo.getArtist();
+        if (!duration) duration = mediainfo.getDuration();
+
+        return this.database.addSound(sender, file, title, artist, duration);
     }
 
-
     /**
-    * Check sound exist in database.
-    *
-    * @param {string} file
-    */
-    async checkExist(file) {
+     * delete sounde
+     *
+     * @param {String} file file ID
+     */
+    async delSound(file) {
         // TODO
     }
+
     /**
     * Add sound to playlist
     *
-    * @param {string} uuid
-    * @param {string} list
+    * @param {String} uuid
+    * @param {String} list
     */
     async addToList(uuid, list) {
         // TOOD
@@ -44,8 +55,8 @@ const Core = class {
     /**
     * Remove sound from playlist
     *
-    * @param {string} uuid
-    * @param {string} list
+    * @param {String} uuid
+    * @param {String} list
     */
     async removeFromList(uuid, list) {
         // TODO
