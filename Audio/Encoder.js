cf3d171ede3b13870b5500164fa23575229b76c6
james58899 <james59988@gmail.com>
2018-01-19 22:18:47 +0800

    fix encoder

--- Audio/Encoder.js
+++ Audio/Encoder.js
@@ -6,6 +6,11 @@ const ffmpeg = require('fluent-ffmpeg');
  * @class encoder
  */
 class encoder {
+    /**
+     * Creates an instance of encoder.
+     * @param {Object} config
+     * @memberof encoder
+     */
     constructor(config) {
         this.config = config;
 
@@ -18,29 +23,32 @@ class encoder {
     }
 
     /**
-    * input file then transcodeing
-    *
-    * @param {any} input
-    * @param {any} filename
-    * @memberof encoder
-    */
+     * input file then transcodeing
+     *
+     * @param {String} input
+     * @param {String} filename
+     * @return {String} Transcoded file path
+     * @memberof encoder
+     */
     async encode(input, filename) {
-        ffmpeg(input)
-            .withNoVideo()
-            .audioFilters('loudnorm')
-            .audioBitrate(this.config.audio.bitrate)
-            .audioCodec('libopus')
-            .duration(this.config.audio.length)
-            .format('opus')
-            .save(path.resolve(this.config.audio.save, filename))
-            .on('error', (err) => {
-                console.log('Cannot process file: ', err.message);
-                throw err;
-            })
-            .on('end', () => {
-                console.log('Succeedful transcoding: ', filename);
-                return path.resolve(this.config.audio.save, filename);
-            });
+        return new Promise((resolve, reject) => {
+            ffmpeg(input)
+                .withNoVideo()
+                .audioFilters('loudnorm')
+                .audioBitrate(this.config.audio.bitrate)
+                .audioCodec('libopus')
+                .duration(this.config.audio.length)
+                .format('opus')
+                .save(path.resolve(this.config.audio.save, filename))
+                .on('error', (err) => {
+                    console.log('Cannot process file: ', err.message);
+                    throw err;
+                })
+                .on('end', () => {
+                    console.log('Succeedful transcoding: ', filename);
+                    resolve(path.resolve(this.config.audio.save, filename));
+                });
+        });
     }
 }
 
