a2f07b03c04d99fadbe2284a6f2668de97a03a08
james58899 <james59988@gmail.com>
2018-02-19 15:17:36 +0800

    two pass normalize

--- Audio/Encoder.js
+++ Audio/Encoder.js
@@ -31,11 +31,20 @@ class Encoder {
      * @memberof encoder
      */
     async encode(input, filename) {
+        const normalize = await this.getNormalize(input);
+
         filename = filename + '.opus';
         return new Promise((resolve, reject) => {
             ffmpeg(input)
                 .withNoVideo()
-                .audioFilters('loudnorm')
+                .audioFilters(
+                    'loudnorm=' +
+                    `measured_I=${normalize.input_i}:` +
+                    `measured_LRA=${normalize.input_lra}:` +
+                    `measured_tp=${normalize.input_tp}:` +
+                    `measured_thresh=${normalize.input_thresh}:` +
+                    `offset=${normalize.target_offset}`
+                )
                 .audioBitrate(this.config.audio.bitrate)
                 .audioCodec('libopus')
                 .duration(this.config.audio.length)
@@ -43,6 +52,21 @@ class Encoder {
                 .save(path.resolve(this.config.audio.save, filename))
                 .on('error', reject)
                 .on('end', () => resolve(filename));
+        });
+    }
+
+    async getNormalize(input) {
+        return new Promise((resolve, reject) => {
+            ffmpeg(input, {
+                stdoutLines: 14
+            })
+                .withNoVideo()
+                .audioFilters('loudnorm=print_format=json')
+                .duration(this.config.audio.length)
+                .format('null')
+                .save('-')
+                .on('error', reject)
+                .on('end', (stdout, stderr) => resolve(JSON.parse(stderr)));
         });
     }
 }
