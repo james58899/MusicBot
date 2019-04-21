41bbe4a0b1915a009e058b023d06e284a5f2a081
james58899 <james59988@gmail.com>
2019-04-21 10:39:39 +0800

    mongo use new url parser

--- dist/Core/MongoDB.js
+++ dist/Core/MongoDB.js
@@ -7,7 +7,7 @@ class MongoDB extends events_1.EventEmitter {
     constructor(config) {
         super();
         config = config.database;
-        mongodb_1.MongoClient.connect(config.host).then(client => {
+        mongodb_1.MongoClient.connect(config.host, { useNewUrlParser: true }).then(client => {
             console.log("[MongoDB] Connected successfully to server");
             this.client = client.db(config.name);
             this.emit("connect", this.client);
