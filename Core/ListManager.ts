7ec90a3d3c8fb4643eaf25e0a6f3c4d805b606f9
james58899 <james59988@gmail.com>
2020-02-01 22:13:00 +0800

    Fix only update one list

--- Core/ListManager.ts
+++ Core/ListManager.ts
@@ -28,7 +28,7 @@ export class ListManager {
             this.database = core.database.client.collection("list");
 
             // Add field admin to old lists
-            this.database.findOneAndUpdate({ admin: { $exists: false } }, { $set: { admin: [] } });
+            this.database.updateMany({ admin: { $exists: false } }, { $set: { admin: [] } });
 
             // Create indexes
             this.database.createIndex({ owner: 1 });
